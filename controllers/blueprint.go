b2abca566e3f8a7bc5a9ef518301391531e4841c
Dmitrii Limonov <disordervlz@gmail.com>
2023-06-01 12:57:29 +0300

    Enhance apply blueprint behaviour

--- controllers/blueprint.go
+++ controllers/blueprint.go
@@ -17,6 +17,7 @@ package controllers
 import (
 	"encoding/json"
 	"github.com/casdoor/casdoor/object"
+	"github.com/casdoor/casdoor/util"
 	"strings"
 	"time"
 )
@@ -37,40 +38,64 @@ func (c *ApiController) ApplyBlueprint() {
 		return
 	}
 
+	//fmt.Printf("%+v", record)
+
 	if record.Action == "update-organization" {
-
-		lastOrganization := object.GetPaginationOrganizations(record.User, 0, 1, "", "", "createdTime", "")
-		organizationName := lastOrganization[0].Name
-
-		isActive := lastOrganization[0].InitScore > 0
-		alreadyHasRole := len(object.GetRoles(organizationName)) > 0
-
-		if !isActive || alreadyHasRole {
+		var org object.Organization
+		if err := json.Unmarshal([]byte(record.Object), &org); err != nil {
+			c.ResponseError(err.Error())
 			return
 		}
+		orgName := "admin/" + org.Name
+
+		currentOrganization := object.GetOrganization(orgName)
+		// prevent double blueprint creation
+		if currentOrganization.BlueprintsApplied {
+			c.ResponseOk("Blueprints already applied")
+			return
+		}
+
+		//key, cert, err := util.GenerateRSACertificate(
+		//	currentOrganization.Name,
+		//	currentOrganization.Name,
+		//	time.Now().Add(10*24*365*time.Hour),
+		//)
 
 		masterRoles := object.GetRoles("built-in")
 		masterModel := object.GetModel("built-in/rbac_built-in") // "rbac_built-in")
 		masterPermissions := object.GetPermissions("built-in")
-		//application := object.GetApplication("admin/app-built-in")
+		applications := object.GetOrganizationApplications("admin", "built-in")
+		plans := object.GetPlans("built-in")
+		pricings := object.GetPricings("built-in")
+		//subscriptions := object.GetSubscriptions("built-in")
 
 		date := time.Now().Format(time.RFC3339)
 
 		//copy model
-		newModel := masterModel
-		newModel.Owner = organizationName
-		newModel.CreatedTime = date
-		object.AddModel(newModel)
+		if masterModel != nil {
+			newModel := masterModel
+			newModel.Owner = currentOrganization.Name
+			newModel.CreatedTime = date
+			object.AddModel(newModel)
+		}
 
 		//copy application
-		//newApplication := application
-		//newApplication.Owner = organizationName
-		//object.AddApplication(newApplication)
+		for _, app := range applications {
+			if app.Name != "app-built-in" {
+				newApp := app
+				newApp.Name = app.Name + "_" + currentOrganization.Name
+				newApp.Organization = currentOrganization.Name
+				newApp.CreatedTime = date
+				newApp.ClientId = util.GenerateClientId()
+				newApp.ClientSecret = util.GenerateClientSecret()
+				object.AddApplication(newApp)
+			}
+		}
 
 		//copy roles
 		for _, role := range masterRoles {
 			newRole := role
-			newRole.Owner = organizationName
+			newRole.Owner = currentOrganization.Name
 			newRole.CreatedTime = date
 			object.AddRole(newRole)
 		}
@@ -85,15 +110,97 @@ func (c *ApiController) ApplyBlueprint() {
 			newPermission.Roles = []string{}
 
 			for _, role := range oldRoles {
-				newPermission.Roles = append(newPermission.Roles, organizationName+"/"+strings.Split(role, "/")[1])
+				newPermission.Roles = append(newPermission.Roles, currentOrganization.Name+"/"+strings.Split(role, "/")[1])
 			}
 
 			//newPermission.Resources = []string{application.Name}
-			newPermission.Owner = organizationName
+			newPermission.Owner = currentOrganization.Name
 			newPermission.CreatedTime = date
 			object.AddPermission(newPermission)
 		}
 
+		// copy plans
+		for _, plan := range plans {
+			plan.Owner = currentOrganization.Name
+			plan.CreatedTime = date
+			object.AddPlan(plan)
+		}
+
+		// copy pricing
+		for _, pricing := range pricings {
+			newPricing := pricing
+			newPricing.Owner = currentOrganization.Name
+			newPricing.CreatedTime = date
+			for i := range pricing.Plans {
+				pricing.Plans[i] = strings.Replace(pricing.Plans[i], "built-in", currentOrganization.Name, -1)
+			}
+			object.AddPricing(newPricing)
+		}
+
+		// copy subscriptions
+		//for _, sub := range subscriptions {
+		//	newSub := sub
+		//	newSub.Owner = currentOrganization.Name
+		//	newSub.State = "Pending"
+		//	object.AddSubscription(newSub)
+		//}
+
+		// create and apply certificate
+		//object.AddCert()
+
+		currentOrganization.BlueprintsApplied = true
+		object.UpdateOrganization(orgName, currentOrganization)
+
+	} else if record.Action == "delete-organization" {
+		var org object.Organization
+		if err := json.Unmarshal([]byte(record.Object), &org); err != nil {
+			c.ResponseError(err.Error())
+			return
+		}
+
+		users := object.GetUsers(org.Name)
+		roles := object.GetRoles(org.Name)
+		model := object.GetModel(org.Name + "/rbac_built-in")
+		permissions := object.GetPermissions(org.Name)
+		applications := object.GetApplications(org.Name)
+		plans := object.GetPlans(org.Name)
+		pricings := object.GetPricings(org.Name)
+		//subscriptions := object.GetSubscriptions(org.Name)
+		//cert := object.GetCert(currentOrganization.Name)
+
+		//for _, sub := range subscriptions {
+		//	object.DeleteSubscription(sub)
+		//}
+
+		for _, pricing := range pricings {
+			object.DeletePricing(pricing)
+		}
+
+		for _, plan := range plans {
+			object.DeletePlan(plan)
+		}
+
+		for _, app := range applications {
+			if app.Organization == org.Name {
+				object.DeleteApplication(app)
+			}
+		}
+
+		object.DeleteModel(model)
+
+		for _, permission := range permissions {
+			object.DeletePermission(permission)
+		}
+
+		for _, role := range roles {
+			object.DeleteRole(role)
+		}
+
+		for _, user := range users {
+			object.DeleteUser(user)
+		}
+
+		//object.DeleteCert(cert)
 	}
 
 	c.ServeJSON()
