8cefaf8b0b13993c640a3068982b65a81a89737b
Gavrishchuk Aleksandr <agavrischuk@ptsecurity.com>
2024-02-20 04:03:15 +0300

    feat: remove old packages

--- util/system_test.go
+++ util/system_test.go
@@ -18,13 +18,8 @@
 package util
 
 import (
-	"path"
-	"runtime"
 	"testing"
 
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/stretchr/testify/assert"
 )
 
