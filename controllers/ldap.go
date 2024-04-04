140c62a6d95fcd1d0f29dbccded26e4099df3d2a
theburyat <theburyat16@gmail.com>
2024-04-04 17:18:33 +0300

    minor fix

--- controllers/ldap.go
+++ controllers/ldap.go
@@ -238,7 +238,7 @@ func (c *ApiController) UpdateLdap() {
 
 	var ldap object.Ldap
 	err := json.Unmarshal(c.Ctx.Input.RequestBody, &ldap)
-	if err != nil || util.IsStringsEmpty(ldap.Owner, ldap.ServerName, ldap.Host, ldap.Username, ldap.Password, ldap.BaseDn) ||
+	if err != nil || util.IsStringsEmpty(ldap.Owner, ldap.ServerName, ldap.Host, ldap.BaseDn) ||
 		!ldap.EnableCryptographicAuth && util.IsStringsEmpty(ldap.Username, ldap.Password) {
 		msg := c.T("general:Missing parameter")
 		record.AddReason(msg)
