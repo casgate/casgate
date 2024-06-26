41e442e619c1467758f16ea29354e6b0d06cd25f
voikin <woysen@gmail.com>
2024-06-26 13:32:00 +0300

    fix: use xorm instead of hard-coded queries

--- object/organization_util.go
+++ object/organization_util.go
@@ -1,10 +1,5 @@
 package object
 
-import (
-	"fmt"
-	"strings"
-)
-
 func HasOrganizationDependencies(orgName string) (bool, error) {
 	tables := []string{
 		"adapter", "application", "cert", "domain", "enforcer", "group", "ldap", "model",
@@ -12,17 +7,15 @@ func HasOrganizationDependencies(orgName string) (bool, error) {
 		"resource", "role", "subscription", "syncer", "token", "user", "webhook",
 	}
 
-	var queryParts []string
 	for _, table := range tables {
-		queryParts = append(queryParts, fmt.Sprintf("SELECT 1 FROM \"%s\" WHERE owner = $1", table))
+		exists, err := ormer.Engine.Table(table).Where("owner = ?", orgName).Exist()
+		if err != nil {
+			return false, err
+		}
+		if exists {
+			return true, nil
+		}
 	}
-	generalQuery := fmt.Sprintf("SELECT EXISTS (%s)", strings.Join(queryParts, " UNION ALL "))
 
-	var exists bool
-	if _, err := ormer.Engine.SQL(generalQuery, orgName).Get(&exists); err != nil {
-		return false, fmt.Errorf("error executing query: %w", err)
-	}
-
-	return exists, nil
+	return false, nil
 }
-
