4d05d8ca4d6ee765299d26ea7569cdd9fa82002a
haze518 <42573582+haze518@users.noreply.github.com>
2024-07-12 15:04:24 +0600

    fix: ensure LDAP users authenticate exclusively via LDAP tab (#233)

--- object/check.go
+++ object/check.go
@@ -420,6 +420,10 @@ func CheckUserPassword(ctx context.Context, organization string, username string
 		}
 	}
 
+	if user.Ldap != "" && !isSigninViaLdap {
+		return nil, fmt.Errorf(i18n.Translate(lang, "check:Go to the LDAP tab and select the specific connection you have configured"))
+	}
+
 	if user.Ldap != "" {
 		if !isSigninViaLdap && !isPasswordWithLdapEnabled {
 			return nil, fmt.Errorf(i18n.Translate(options.Lang, "check:password or code is incorrect"))
